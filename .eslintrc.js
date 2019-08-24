19077b2e9ce8a38b044da0f3579b2d1be953d2ff
Checkson <checkson@liangqiushengdeMacBook-Pro.local>
2019-08-24 10:16:56 +0800

    change config format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    root: true,
-    extends: 'standard',
-    parserOptions: {
-        sourceType: 'module'
-    },
-    env: {
-        node: true,
-        jest: true
-    },
-    rules: {
-        "indent": ["error", 2],
-        "quotes": ["error", "single"],
-        "semi": ["error", "always"],
-        "no-console": "off",
-        "arrow-parens": 0,
-        "no-new": "off"
-    }
+  root: true,
+  extends: 'standard',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    jest: true
+  },
+  rules: {
+    "indent": ["error", 2],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "no-console": "off",
+    "arrow-parens": 0,
+    "no-new": "off"
+  }
 }
